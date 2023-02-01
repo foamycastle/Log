49454e639d4f4447348323a1d67766f3dea047ae
Aaron Sollman <74476626+foamycastle@users.noreply.github.com>
2023-02-01 16:47:39 -0600

    Composer Updated

--- src/Exception/CannotCreatePathException.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-namespace FoamyCastle\Log\Exception;
-
-class CannotCreatePathException extends \Exception {
-	public function __construct(string $method,string $file,int $line) {
-		parent::__construct("$file $method line:$line Cannot create new path for log file");
-	}
-}
